b51f4759b1ef90e96036e9c9c94c60f66956c95d
Andres Hortua <fandresh7@gmail.com>
2025-03-11 20:03:47 -0500

    feat: migrate select-dropdown component to multi-select-dropdown with updated template and styles

--- src/app/controls/multi-select-dropdown/multi-select-dropdown.component.ts
+++ src/app/controls/multi-select-dropdown/multi-select-dropdown.component.ts
@@ -1,22 +1,57 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
-import { SelectDropdownComponent } from '../../components/select-dropdown/select-dropdown.component'
-import { BaseInputComponent } from '../base-input/base-input.component'
-import { FormControl } from '@angular/forms'
-import { Control } from '../../interfaces/forms.interfaces'
-import { from, Observable, of } from 'rxjs'
+import { ChangeDetectionStrategy, Component, signal, effect } from '@angular/core'
+import { SelectionModel } from '@angular/cdk/collections'
+import { OverlayModule } from '@angular/cdk/overlay'
 import { toSignal } from '@angular/core/rxjs-interop'
+import { from, map, Observable, of } from 'rxjs'
+import { FormControl } from '@angular/forms'
+
+import { BaseInputComponent, controlDeps } from '../base-input/base-input.component'
+import { Control } from '../../interfaces/forms.interfaces'
 
 @Component({
   selector: 'multi-select-dropdown',
-  imports: [SelectDropdownComponent],
+  imports: [...controlDeps, OverlayModule],
   templateUrl: './multi-select-dropdown.component.html',
   styleUrl: './multi-select-dropdown.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '(click)': 'open()'
+  }
 })
 export class MultiSelectDropdownComponent<T> extends BaseInputComponent {
   override formControl = new FormControl(Array.isArray(this.value()) ? this.value() : [], this.validatorFn)
 
   items = toSignal(this.getItems(), { initialValue: [] })
+
+  isOpen = signal<boolean>(false)
+  selectedOptionsModel = new SelectionModel<T>(true)
+  displayOptions = toSignal(this.selectedOptionsModel.changed.pipe(map(() => this.selectedOptionsModel.selected)), { initialValue: [] })
+
+  constructor() {
+    super()
+
+    effect(() => {
+      this.setSelectedItems(this.items())
+    })
+
+    effect(() => {
+      const selected = this.displayOptions()
+      this.valueHasChanged(selected)
+    })
+  }
+
+  setSelectedItems(items: T[]): void {
+    const formControlValue = this.formControl.value
+    this.selectedOptionsModel.clear()
+
+    if (Array.isArray(formControlValue)) {
+      items.forEach(item => {
+        if (formControlValue.some((val: string) => this.compareWithFn(item, val))) {
+          this.selectedOptionsModel.select(item)
+        }
+      })
+    }
+  }
 
   getItems(): Observable<T[]> {
     const control = this.control() as Control<T>
@@ -31,7 +66,7 @@ export class MultiSelectDropdownComponent<T> extends BaseInputComponent {
     return of([])
   }
 
-  displayWithFn = (item: T) => {
+  displayWithFn(item: T) {
     const control = this.control() as Control<T>
 
     if (control.itemLabel) {
@@ -41,7 +76,7 @@ export class MultiSelectDropdownComponent<T> extends BaseInputComponent {
     }
   }
 
-  compareWithFn = (item: T, value: string) => {
+  compareWithFn(item: T, value: string) {
     const control = this.control() as Control<T>
 
     if (control.itemValue) {
@@ -65,5 +100,22 @@ export class MultiSelectDropdownComponent<T> extends BaseInputComponent {
     })
 
     this.formControl.setValue(values)
+  }
+
+  open() {
+    this.isOpen.set(true)
+  }
+
+  close() {
+    this.isOpen.set(false)
+  }
+
+  toggleSelection(option: T) {
+    this.selectedOptionsModel.toggle(option)
+  }
+
+  deselectOption(event: MouseEvent, option: T) {
+    event.stopPropagation()
+    this.selectedOptionsModel.deselect(option)
   }
 }
