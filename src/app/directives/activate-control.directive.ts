212b33fe2521233339eb9606994497f2fa5fdbe9
Andres Hortua <fandresh7@gmail.com>
2025-10-05 12:02:35 -0500

    refactor: migrate activate control directive from RxJS to signals

--- src/app/directives/activate-control.directive.ts
+++ src/app/directives/activate-control.directive.ts
@@ -1,44 +1,30 @@
-import { Directive, inject, input, OnDestroy, OnInit, TemplateRef, ViewContainerRef } from '@angular/core'
+import { computed, Directive, effect, inject, input, TemplateRef, ViewContainerRef } from '@angular/core'
+import { toSignal } from '@angular/core/rxjs-interop'
 import { FormGroupDirective } from '@angular/forms'
-import { map, startWith, Subscription } from 'rxjs'
+import { startWith } from 'rxjs'
 
 import { Control } from '../interfaces/forms.interfaces'
 
 @Directive({
   selector: '[activateControl]'
 })
-export class ActivateControlDirective implements OnInit, OnDestroy {
+export class ActivateControlDirective {
   activateControl = input<Control>()
 
   private vcr = inject(ViewContainerRef)
   private templateRef = inject(TemplateRef)
   private rootFormGroup = inject(FormGroupDirective).form
-  private subscription!: Subscription
 
-  ngOnInit(): void {
-    this.subscription = this.rootFormGroup.valueChanges
-      .pipe(
-        startWith(this.rootFormGroup.value),
-        map(() => this.isVisible())
-      )
-      .subscribe(isVisible => {
-        if (isVisible) {
-          if (this.vcr.length === 0) {
-            this.vcr.createEmbeddedView(this.templateRef)
-          }
-        } else {
-          if (this.vcr.length > 0) {
-            this.vcr.clear()
-          }
-        }
-      })
-  }
+  // ✅ Convertir valueChanges a signal
+  private formValue = toSignal(this.rootFormGroup.valueChanges.pipe(startWith(this.rootFormGroup.value)), {
+    initialValue: this.rootFormGroup.value
+  })
 
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe()
-  }
+  // ✅ Computed para visibilidad
+  private isVisible = computed(() => {
+    // Trigger reactivity cuando cambia el form
+    const _ = this.formValue()
 
-  isVisible() {
     const visible = this.activateControl()?.visible
 
     if (visible === undefined) {
@@ -50,5 +36,24 @@ export class ActivateControlDirective implements OnInit, OnDestroy {
     }
 
     return visible
+  })
+
+  constructor() {
+    // ✅ Effect para manejar el DOM
+    effect(() => {
+      const visible = this.isVisible()
+
+      if (visible) {
+        if (this.vcr.length === 0) {
+          this.vcr.createEmbeddedView(this.templateRef)
+        }
+      } else {
+        if (this.vcr.length > 0) {
+          this.vcr.clear()
+        }
+      }
+    })
   }
+
+  // ✅ Ya no necesitas ngOnInit ni ngOnDestroy
 }
